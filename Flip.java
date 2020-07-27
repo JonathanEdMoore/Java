8ccc179a94cc008c6e5ca0e6b9af2afaea4cb919
Jonathan Moore <jonathan.moore6247@gmail.com>
2020-07-27 15:10:28 -0500

    Added Five Integers

--- Flip.java
+++ Flip.java
@@ -2,7 +2,13 @@ public class Flip
 {
 	public static void main(String[] args)
 	{
-		if (Math.random() < 0.5) System.out.println("Heads");
-		else					 System.out.println("Tails");
+		if (Math.random() < 0.5) 
+		{
+			System.out.println("Heads");
+		}
+		else
+		{
+			System.out.println("Tails");
+		}
 	}
 }
