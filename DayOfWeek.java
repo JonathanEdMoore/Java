9c47dd5ea44b990120511dbb0b20e9c41dd8973c
Jonathan Moore <jonathan.moore6247@gmail.com>
2020-07-27 18:17:48 -0500

    Updated DayOfWeek

--- DayOfWeek.java
+++ DayOfWeek.java
@@ -11,13 +11,15 @@ public class DayOfWeek
 		int m0 = m + 12 * ((14 - m) / 12) - 2;
 		int d0 = (d + x + (31*m0)/12) % 7;
 		
-		if (d0 == 0) System.out.println("Sunday");
-		else if (d0 == 1) System.out.println("Monday");
-		else if (d0 == 2) System.out.println("Tuesday");
-		else if (d0 == 3) System.out.println("Wednesday");
-		else if (d0 == 4) System.out.println("Thursday");
-		else if (d0 == 5) System.out.println("Friday");
-		else if (d0 == 6) System.out.println("Saturday");
-	
+		switch (d0)
+		{
+			case 0: System.out.println("Sunday"); break;
+			case 1: System.out.println("Monday"); break;
+			case 2: System.out.println("Tuesday"); break;
+			case 3: System.out.println("Wednesday"); break;
+			case 4: System.out.println("Thursday"); break;
+			case 5: System.out.println("Friday"); break;
+			case 6: System.out.println("Saturday"); break;
+		}
 	}
 }
