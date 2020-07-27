927a34d474ae521d299291a73c8a99f5ef4c1b1f
Jonathan Moore <jonathan.moore6247@gmail.com>
2020-07-26 19:14:23 -0500

    Updated Quadratic

--- Quadratic.java
+++ Quadratic.java
@@ -2,11 +2,23 @@ public class Quadratic
 {
 	public static void main(String[] args)
 	{
-		double b = Double.parseDouble(args[0]);
-		double c = Double.parseDouble(args[1]);
-		double discriminant = b*b - 4.0*c;
+		double a = Double.parseDouble(args[0]);
+		double b = Double.parseDouble(args[1]);
+		double c = Double.parseDouble(args[2]);
+		double discriminant = b*b - 4.0*a*c;
 		double d = Math.sqrt(discriminant);
-		System.out.println((-b + d) / 2.0);
-		System.out.println((-b - d) / 2.0);
+
+		if(a == 0)
+		{
+			System.out.println("'a' can not be zero");
+		}
+		else if(discriminant < 0)
+		{
+			System.out.println("Discriminant is negative");
+		}
+		else{
+			System.out.println((-b + d) / 2.0*a);
+			System.out.println((-b - d) / 2.0*a);
+		}
 	}
 }
